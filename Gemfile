0a237614e3c560d33230679f7b66bfec9d492893
David Potsiadlo <davidpots@gmail.com>
2012-12-02 00:55:30 -0500

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+gem 'pg', '~>0.13.2'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Songnotes::Application.config.secret_token = 'c982bf29725af315dfbf6f016d6a3f0c89c94e6e38b47159ca07ff21d4ebfcd19efc14224338d154eee59a8b075eb808643d4228ffc6b415888a3bee200ce185'
