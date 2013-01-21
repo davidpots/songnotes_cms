c6f33510c8b8dc7a92ba728b53b09eac92b58e14
David Potsiadlo <davidpots@gmail.com>
2013-01-20 23:01:55 -0500

    Adds Google Analytics code to main layout. Adds title for main layout (and the song details view).

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,14 @@
 module ApplicationHelper
+
+  # Sort Helper used in Admin Index
   def sortable(column, title = nil)
     title ||= column.titleize
     direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
     link_to title, :sort => column, :direction => direction
   end
+
+  def title(page_title)
+    content_for(:title) { page_title }
+  end
+
 end
