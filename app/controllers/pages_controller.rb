50ea10d21b4813558372df68e775602137d719e1
David Potsiadlo <davidpots@gmail.com>
2013-02-10 21:14:27 -0500

    Shuffles things around on homepage.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
   layout 'redux'
   def home
-    @songs_by_months = Song.where(published: true).all(:order => 'created_at DESC', :limit => 5)
-    @songs_random = Song.where(published: true).all(:order => 'created_at DESC').sample(5)
+    @songs_by_months = Song.where(published: true).all(:order => 'created_at DESC', :limit => 7)
+    @songs_random = Song.where(published: true).all(:order => 'created_at DESC').sample(7)
     @song_count = Song.where(published: true).count
   end
 end
