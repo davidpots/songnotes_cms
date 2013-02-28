dd02b30c5d33ea9c2adea9fbe4e17788f5918dc8
David Potsiadlo <davidpots@gmail.com>
2013-02-28 13:37:31 -0500

    Adds Nokogiri and support for parsing out lyrics from the HTML into plain text. Also adds 'WIth a little help from my friends' song.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,4 +5,19 @@ class PagesController < ApplicationController
     @songs_random = Song.where(published: true).all(:order => 'created_at DESC').sample(7)
     @song_count = Song.where(published: true).count
   end
+  def sandbox
+    @song = Song.find(5)
+    html_doc = Nokogiri::XML(@song.lyrics_html_mono)
+    html_doc.xpath('//strong').each {|x| x.remove}
+    html_doc.xpath('//span').each {|x| x.remove}
+    html_doc.xpath('//h4').each {|x| x.remove}
+    @doc = html_doc.to_s
+    # Gets rid of spaces
+    @new_doc = @doc.gsub("\s\s\s\s","")
+    # Gets rid of duplicate line breaks
+    @new_doc = @new_doc.gsub("\n\n\n","\n\n")
+    # Gets rid of leading line break (start of text)... SEEMS LIKE COULD BE WAY BETTER!
+    @new_doc = @new_doc.sub("\n","")
+    render :layout => 'sandbox'
+  end
 end
