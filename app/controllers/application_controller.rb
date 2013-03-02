ae952ad0354b956f0f41064bdf94bba31d0d623b
David Potsiadlo <davidpots@gmail.com>
2013-03-01 22:29:55 -0500

    Design overhaul! Bigger type. Backgorund color change. New fonts.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :layout_vars
   def layout_vars
+    @google_font_Bitter = "Bitter:400,700,400italic"
+    @google_font_Anonymous = "Anonymous+Pro:400,700"
     @google_font_OpenSans = "Open+Sans:300italic,400italic,600italic,700italic,400,700,600,300"
     @google_font_AveriaSerifLibre = "Averia+Serif+Libre:300,400,700,300italic,400italic,700italic"
     @google_font_SourceCodePro = "Source+Code+Pro:300,400,600,700"  
