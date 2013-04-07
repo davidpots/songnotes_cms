d0664f931e4edacf4eb30e1559e2361fa6e793c8
David Potsiadlo <davidpots@gmail.com>
2013-04-07 09:21:02 -0400

    Adds 'Flake' by Jack Johnson

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,5 @@
 class Song < ActiveRecord::Base
-  attr_accessible :artist_id, :album_id, :lyrics, :title, :year, :lyrics_text, :lyrics_html_mono, :chords, :repeated_chords, :published, :notes, :record_label, :slug
+  attr_accessible :artist_id, :album_id, :lyrics, :title, :year, :lyrics_text, :lyrics_html_mono, :chords, :repeated_chords, :published, :notes, :record_label, :slug, :key, :lyrics_raw, :lyrics_signature, :url_wikipedia, :url_youtube_official, :url_rdio
   belongs_to :artist
   belongs_to :album
   def to_param
