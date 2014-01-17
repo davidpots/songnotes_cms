9d8e853476f3739a6f8942e8020f79a6ae1115c2
David Potsiadlo <davidpots@gmail.com>
2014-01-17 09:53:46 -0500

    Fixes by-year secondary sort order (title) on song page

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -15,7 +15,7 @@ class SongsController < ApplicationController
     @recent_songs = Song.where(published: true).all(order: 'created_at DESC', limit: 6)
     @song_count = Song.where(published: true).count
     #@songs = Song.where(published: true).all(order: 'lower(title)')
-    @songs_by_year = Song.all(order: 'year DESC').group_by { |song| song.year }
+    @songs_by_year = Song.all(order: 'year DESC, title').group_by { |song| song.year }
 
   end
   
